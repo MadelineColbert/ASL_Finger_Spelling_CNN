5df9cfd844c075fd4920c73e771c77c26b24b2d1
MadelineColbert <madeline.r.colbert+eglrsaf4@gmail.com>
2025-08-28 11:28:23 -0500

    Saving final initial changes

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import torch
 from src.model_training import training_loop
 # from src.data_handling import load_data
+import mlflow
+from dotenv import load_dotenv
+import os
 
 def main():
+    load_dotenv()
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_URI"))
+    mlflow.set_experiment("asl_finger_spelling")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = training_loop(device)
     print("TRAINING FINISHED")
--- src/model_training.py
+++ src/model_training.py
@@ -15,7 +15,9 @@ def training_loop(device):
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
-    model = train(model, criterion, optimizer, train_data, device)
+    with mlflow.start_run():
+        model = train(model, criterion, optimizer, train_data, device)
+        test_valid = validation(model, valid_data, device)
     return model
 
 
@@ -24,49 +26,65 @@ def train(model:VisionModel, criterion:nn.CrossEntropyLoss, optimizer:optim.Adam
     running_loss = 0
     total_correct = 0
     total_used = 0
-    with mlflow.start_run():
-            
-        for epoch in range(2):
-            for i, data in enumerate(train_data, 0):
-                inputs, labels = data
-                inputs, labels = inputs.to(device), labels.to(device)
+    for epoch in range(2):
+        for i, data in enumerate(train_data, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
-                #Add in MLFlow for monitoring
-                optimizer.zero_grad()
-                outputs = model.forward(inputs)
+            #Add in MLFlow for monitoring
+            optimizer.zero_grad()
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, labels)
 
-                predictions = F.softmax(outputs).argmax(dim=1)
+            predictions = F.softmax(outputs).argmax(dim=1)
 
-                loss = criterion(outputs, labels) # Move Criterion to model?
-                accuracy_vec = predictions == labels
-                total_correct += accuracy_vec.sum()
-                total_used += accuracy_vec.shape[0]
-                loss.backward()
-                optimizer.step()
+            accuracy_vec = predictions == labels
+            total_correct += accuracy_vec.sum()
+            total_used += accuracy_vec.shape[0]
+            loss.backward()
+            optimizer.step()
 
-                #Check in on the loss
-                running_loss += loss.item()
-                if i%100 == 99:
-                    loss = running_loss/100
-                    accuracy = total_correct/total_used
-                    mlflow.log_metrics(
-                        {"batch_loss": loss, "batch_accuracy": accuracy},
-                        step=epoch * len(train_data) + i
-                    )
-                    running_loss = 0
-                    total_correct = 0
-                    total_used = 0
-        
-        mlflow.pytorch.log_model(model)
+            #Check in on the loss
+            running_loss += loss.item()
+            if i%100 == 99:
+                loss = running_loss/100
+                accuracy = total_correct/total_used
+                mlflow.log_metrics(
+                    {"train_loss": loss, "train_accuracy": accuracy},
+                    step=epoch * len(train_data) + i
+                )
+                running_loss = 0
+                total_correct = 0
+                total_used = 0
+    
+    mlflow.pytorch.log_model(model, name="model")
     return model
 
-def test(model:VisionModel, test_data):
+def validation(model:VisionModel, criterion: nn.CrossEntropyLoss, valid_data, device):
     model.eval()
+    running_loss = 0
+    total_correct = 0
+    total_used = 0
     with torch.no_grad():
-        for i, data in enumerate(test_data, 0):
-            pass
+        for i, data in enumerate(valid_data, 0):
             inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
             results = model(inputs)
+            loss = criterion(results, labels)
 
-    accuracy = 0
-    return accuracy
+            results = F.softmax(results).argmax(dim=1)
+
+            accuracy_vec = results == labels
+            total_correct += accuracy_vec.sum()
+            total_used += accuracy_vec.shape[0]
+
+            if i%100 == 99:
+                loss = running_loss/100
+                accuracy = total_correct/total_used
+                mlflow.log_metrics(
+                    {"valid_loss": loss, "valid_accuracy": accuracy},
+                    step=i
+                )
+                running_loss = 0
+                total_correct = 0
+                total_used = 0
